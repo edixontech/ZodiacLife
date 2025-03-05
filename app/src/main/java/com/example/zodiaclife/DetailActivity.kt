17b622d3f7ebee0ce8f6274d9036b161e4709137
mananas <tecnoedix@icloud.com>
2025-03-05 11:12:23 +0100

    correcion errores, activar menu , iconos
    visto Intent, nuevas variables

--- app/src/main/java/com/example/zodiaclife/DetailActivity.kt
+++ app/src/main/java/com/example/zodiaclife/DetailActivity.kt
@@ -1,17 +1,29 @@
-package com.example.zodiaclife
+package com.example.zodiac
 
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.zodiaclife.Horoscope
+import com.example.zodiaclife.R
 
 class DetailActivity : AppCompatActivity() {
 
     companion object {
         const val EXTRA_HOROSCOPE_ID = "HOROSCOPE_ID"
     }
+
+    lateinit var nameTextView: TextView
+    lateinit var dateTextView: TextView
+    lateinit var iconImageView: ImageView
+
+    lateinit var horoscope: Horoscope
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,10 +39,44 @@ class DetailActivity : AppCompatActivity() {
         }
 
         val id = intent.getStringExtra(EXTRA_HOROSCOPE_ID)!!
+        horoscope = Horoscope.findById(id)
 
+        initView()
 
+        loadData()
+    }
 
-        val horoscope = Horoscope.findById(id)
-        findViewById<TextView>(R.id.text).text = "id: ${getString(horoscope.name)}"
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_activity_detail, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_favorite -> {
+                Log.i("MENU", "Menu favorito")
+                true
+            }
+            R.id.action_share -> {
+                Log.i("MENU", "Menu compartir")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun loadData() {
+        supportActionBar?.setTitle(horoscope.name)
+        supportActionBar?.setSubtitle(horoscope.dates)
+
+        nameTextView.setText(horoscope.name)
+        dateTextView.setText(horoscope.dates)
+        iconImageView.setImageResource(horoscope.icon)
+    }
+
+    private fun initView() {
+        nameTextView = findViewById(R.id.nameTextView)
+        dateTextView = findViewById(R.id.dateTextView)
+        iconImageView = findViewById(R.id.iconImageView)
     }
 }
--- app/src/main/java/com/example/zodiaclife/MainActivity.kt
+++ app/src/main/java/com/example/zodiaclife/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.zodiac.DetailActivity
 
 
 class MainActivity : AppCompatActivity() {
